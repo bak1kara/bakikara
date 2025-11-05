98310f3c5eebc201fcbc7eff7b6dbdc3d5d55fa4
baki kara <nicewozzy@gmail.com>
2025-11-05 15:20:41 +0300

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,23 +1,22 @@
 // firebase-config.js
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js";
-import { getFirestore, doc, setDoc, getDoc } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js";
+// Firestore'dan EK ALANLAR EKLENDİ
+import { getFirestore, doc, setDoc, getDoc, collection, getDocs, updateDoc } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js";
 
-// SİZİN Firebase Yapılandırma Bilgileriniz
+
+// Firebase Config Objesi buraya geliyor (apiKey, authDomain, vs.)
 const firebaseConfig = {
-    apiKey: "AIzaSyATqhJsWJGv3AxdvLmCKS-T9UlDycwhXys",
-    authDomain: "baki-s2-hosting.firebaseapp.com",
-    projectId: "baki-s2-hosting",
-    storageBucket: "baki-s2-hosting.firebasestorage.app",
-    messagingSenderId: "1096636788468",
-    appId: "1:1096636788468:web:ef3365f47930ab98a738a1",
-    measurementId: "G-3QDCRYR2RE"
+    // ... Sizin projenizin bilgileri burada olmalı ...
 };
+
 
 // Firebase Servislerini Başlat
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app); 
 const db = getFirestore(app); 
+
 
 // Gerekli tüm fonksiyonları ve servisleri dışa aktar
 export { 
@@ -30,5 +29,9 @@ export {
     onAuthStateChanged, 
     doc, 
     setDoc,
-    getDoc
+    getDoc,
+    // YENİ EKLENEN Firestore Fonksiyonları:
+    collection, 
+    getDocs, 
+    updateDoc 
 };
